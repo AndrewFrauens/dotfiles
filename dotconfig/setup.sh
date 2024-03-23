3f254692e77b1694c11dda70082af76ba785a93d
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2024-03-23 16:11:14 -0400

    added go .profile

--- dotconfig/setup.sh
+++ dotconfig/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # make sure alacritty and nvim actually need the extra dir layer
-ln -s ~/dotfiles/dotconfig/alacritty ~/.config/alacritty
-ln -s ~/dotfiles/dotconfig/nvim ~/.config/nvim
+ln -s ~/dotfiles/dotconfig/alacritty ~/.config
+ln -s ~/dotfiles/dotconfig/nvim ~/.config
 ln -s ~/dotfiles/dotconfig/helix ~/.config
 
--- home/setup.sh
+++ home/setup.sh
@@ -1,4 +1,5 @@
 cp ./.bashrc ~/.bashrc
+cp ./.profile ~/.profile
 cp -r ./.tmux ~/.tmux
 cp ./.tmux.conf ~/.tmux.conf
 
