6af715e3cae23aed29f4cffefc0c5c70939b4d0f
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2024-03-29 23:32:04 -0400

    setup a wide variety of common lsp that might be needed

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -542,17 +542,37 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
+				clangd = {},
+				gopls = {},
+				pyright = {},
+				rust_analyzer = {},
+				bashls = {},
+				cmake = {},
+				cssls = {},
+				docker_compose_language_service = {},
+				dockerls = {},
+				emmet_language_server = {},
+				eslint = {},
+				html = {},
+				jsonls = {},
+				ltex = {},
+				ocamlls = {},
+				postgres_lsp = {},
+				sqlls = {},
+				tailwindcss = {},
+				terraform_lsp = {},
+				zls = {},
+				wgsl_analyzer = {},
+				glslls = {},
+				graphql = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
+				tsserver = {},
+
 				--
 
 				lua_ls = {
